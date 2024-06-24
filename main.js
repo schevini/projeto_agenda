308bc810d156b28a16644b7423fdbe7591e3269e
vini.sche <vini.sche@live.com>
2024-06-24 11:18:10 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,23 @@
+const form = document.getElementById('form-atividade');
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNome = document.getElementById('nome');
+    const inputTelefone = document.getElementById('telefone');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputTelefone.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas;
+
+    inputNome.value = '';
+    inputTelefone.value = '';
+
+});
